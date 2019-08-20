1f7d8fc5ff2f077b9a30184d4c4502578430ad12
Projovi Macbook Air <developers@projovi.co>
2019-08-20 21:07:27 +0530

    Files Update

--- config/projovieazypay.php
+++ config/projovieazypay.php
@@ -17,15 +17,15 @@ return [
     |------------------------------------------------------------------------------------
     |
     | Don't confused with Reference No (merchant_reference_no)
-    |   - one reference_no is used in mandatory field in url that stands for order_id or any 
+    |   - one reference_no is used in mandatory field in url that stands for order_id or any
     |     other refence no(id)
-    |   - in url there is field of Reference No which means we used our encrpted 
+    |   - in url there is field of Reference No which means we used our encrpted
     |     merchant_reference_no which stands for our merchant_id which is used in config file
     |
     */
 
     // in url encryption (Reference No) is same as merchant id change value in case need
-    'merchant_reference_no'      =>  env('EAZYPAY_MERCHANT_ID'),     
+    'merchant_reference_no'      =>  env('EAZYPAY_MERCHANT_ID'),
 
     // paymode value must be 9 according to icici payment integration document
     'paymode'           =>  env('EAZYPAY_PAYMODE',9),
